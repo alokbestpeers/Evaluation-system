0bdd7bda5be10b8b5feb974fc0d07c202f0674a1
alokbestpeers <aramteke@bestpeers.com>
2018-09-05 20:24:05 +0530

    register models

--- test_system/admin.py
+++ test_system/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Candidate, Questions, Technology
+
 admin.site.register(Candidate)
 admin.site.register(Questions)
 admin.site.register(Technology)
