aaddb39c8b92d1ccc034b5972b9c65d88d3d39d8
alokbestpeers <aramteke@bestpeers.com>
2018-09-05 20:26:38 +0530

    created views for technology and questions

--- test_system/views.py
+++ test_system/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import CandidateForm
+from .models import Technology, Questions
 
 def home(request):
-	return render(request, 'home.html')
+    return render(request, 'home.html')
 
 def candidate_detail(request):
-	form = CandidateForm()
-	return render(request, 'candidate_detail.html', {'form':form})
+    form = CandidateForm()
+    # import pdb; pdb.set_trace()
+    if request.method == 'POST':
+        if form.is_valid():
+            form = CandidateForm(request.POST)
+            form.save()
+    else:
+        form = CandidateForm()
+    return render(request, 'candidate_detail.html', {'form':form})
+
+def technology(request):
+    technology = Technology.objects.all()
+    return render(request, 'technology.html', {'technology':technology})
+
+def tech_questions(request, pk):
+    questions = Questions.objects.get(technology=pk)
+    return render(request, 'tech_questions.html', {'questions':questions})
