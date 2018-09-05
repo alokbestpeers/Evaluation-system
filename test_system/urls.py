bf6049d0b1d92cd254463c2d33a6fa39809596aa
alokbestpeers <aramteke@bestpeers.com>
2018-09-05 20:25:25 +0530

    added urls for technology page and its questions

--- test_system/urls.py
+++ test_system/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home' ),
     url(r'^candidate/$', views.candidate_detail, name='candidate_detail'),
+    url(r'^technology/$', views.technology, name='technology'),
+    url(r'^technology/(?P<pk>\d+)/$', views.tech_questions, name='tech_questions'),
 ]
