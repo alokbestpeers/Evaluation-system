00f3c40cac807c1f7152a41bbc072491569b716e
alokbestpeers <aramteke@bestpeers.com>
2018-09-06 14:59:02 +0530

    removed candidate detail view

--- test_system/views.py
+++ test_system/views.py
@@ -1,25 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import CandidateForm
 from .models import Technology, Questions
 
 def home(request):
-    return render(request, 'home.html')
-
-def candidate_detail(request):
     form = CandidateForm()
     # import pdb; pdb.set_trace()
     if request.method == 'POST':
+        form = CandidateForm(request.POST)
         if form.is_valid():
-            form = CandidateForm(request.POST)
             form.save()
+            return redirect('technology')
     else:
         form = CandidateForm()
-    return render(request, 'candidate_detail.html', {'form':form})
+    return render(request, 'home.html', {'form':form})
 
 def technology(request):
     technology = Technology.objects.all()
     return render(request, 'technology.html', {'technology':technology})
 
 def tech_questions(request, pk):
-    questions = Questions.objects.get(technology=pk)
+    questions = Questions.objects.filter(technology=pk)
     return render(request, 'tech_questions.html', {'questions':questions})
